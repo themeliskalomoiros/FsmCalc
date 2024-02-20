edad0dd8de3f70825e08e590101a1acacff87ca7
Themelis Kalomoiros <arrditis@gmail.com>
2024-02-20 10:11:20 +0000

    init commit

---
+++ CalculatorApp/Form1.cs
@@ -0,0 +1,9 @@
+namespace CalculatorApp;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CalculatorTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CalculatorTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
