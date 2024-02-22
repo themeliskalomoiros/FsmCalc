96c920d62e34c1f7cfdc8aa0d7e9742d00a26b02
Themelis Kalomoiros <arrditis@gmail.com>
2024-02-22 14:50:36 +0000

    CalculatorEngine process an input and raises an event if its output value changed.

--- Calculator/CalculatorEngine.cs
+++ Calculator/CalculatorEngine.cs
@@ -1,9 +1,50 @@
-﻿namespace Calculator;
+using System.Text;
+
+namespace Calculator;
 
 public class CalculatorEngine
 {
-	public string GetOutput(Alphabet input)
+	public event EventHandler<CalculatorEngineEventArgs>? OutputValueChanged;
+	
+	private string Output { get; set; }
+	private State CurrentState { get; set; }
+
+	public CalculatorEngine()
 	{
-		throw new ArgumentException($"{input} is not a valid argument for this calculator.");
+		Output = "0";
+		CurrentState = State.A;
+	}
+
+	public void Process(Alphabet input)
+	{
+		ThrowForInvalidInput(input);
+
+		CurrentState = StateMachine.GetNextState(CurrentState, input);
+
+		if (CurrentState == State.B && input.IsNumeric())
+		{
+			if (input == Alphabet.Zero && double.TryParse(Output, out var result))
+			{
+				if (result == 0.0)
+					return;
+			}
+			else
+			{
+				Output += input.ToString();
+				if (OutputValueChanged != null)
+				{
+					OutputValueChanged(this, new CalculatorEngineEventArgs(Output));	
+				}
+			}
+		}
+	}
+
+	private void ThrowForInvalidInput(Alphabet input)
+	{
+		if (!Enum.IsDefined(typeof(Alphabet), input))
+		{
+			throw new ArgumentException(
+				$"{input} is not a valid argument for this calculator.");
+		}
 	}
 }
--- CalculatorTests/CalculatorEngineShould.cs
+++ CalculatorTests/CalculatorEngineShould.cs
@@ -4,12 +4,33 @@ namespace CalculatorTests;
 
 public class CalculatorEngineShould
 {
+	private readonly CalculatorEngine _sut;
+
+	public CalculatorEngineShould()
+	{
+		_sut = new CalculatorEngine();
+	}
+
 	[Fact]
 	public void ThrowForInvalidInput()
 	{
-		var sut = new CalculatorEngine();
 		var invalidInput = (Alphabet) 99;
+		Action testCode = () => _sut.Process(invalidInput);
+		Assert.Throws<ArgumentException>(testCode);
+	}
 
-		Assert.Throws<ArgumentException>(() => sut.GetOutput(invalidInput));
+	[Theory]
+	[InlineData(Alphabet.One)]
+	public void RaisesWhenOutputChangesValue(params Alphabet[] seriesOfInputs)
+	{
+		Action processInputs = () => {
+			foreach (var input in seriesOfInputs) 
+				_sut.Process(input);
+		};
+
+		Assert.Raises<CalculatorEngineEventArgs>(
+			handler => _sut.OutputValueChanged += handler,
+			handler => _sut.OutputValueChanged -= handler,
+			processInputs);
 	}
 }
